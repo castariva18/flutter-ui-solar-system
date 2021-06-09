de95819608d76e39f28dbc98b6e5afb25fa1fa91
Belmiro <tengku13elmiro@gmail.com>
2021-06-10 01:00:13 +0700

    IconLaucher

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Solar System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
